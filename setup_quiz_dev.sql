4024361ea84d3b66a47c70410bcc1226cd7586d7
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-06-27 23:03:36 +0100

    Add sql file

---
+++ setup_quiz_dev.sql
@@ -0,0 +1,13 @@
+--- This file is used to setup the database for the quiz app.
+--- A user is created with the name quiz_app_user and password quiz_app_password.
+--- The user is granted all privileges on the quiz_app_db database.
+--- The user is granted select privileges on the performance_schema database.
+--- The database is created if it does not exist.
+--- The user is created if it does not exist.
+
+
+CREATE DATABASE IF NOT EXISTS quiz_app_db;
+CREATE USER IF NOT EXISTS 'quiz_app_user'@'localhost' IDENTIFIED BY 'quiz_app_password';
+GRANT ALL PRIVILEGES ON quiz_app_db.* TO 'quiz_app_user'@'localhost';
+GRANT SELECT ON perfomance_schema.* TO 'quiz_app_user'@'localhost';
+FLUSH PRIVILEGES;
