78e91fa00120e9d02557136bb68da1183b3417d3
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-07-07 01:23:04 +0100

    Update

--- insert_users.py
+++ insert_users.py
@@ -4,11 +4,15 @@ This script is used to insert into the database
 aside using the API
 """
 from sys import argv
-from models.subject import Subject
+from models.user import User
 from models.base_model import app, db
 
 with app.app_context():
-    subject = Subject(
-        name=argv[1]
+    user = User(
+        username=argv[1],
+        first_name=argv[2],
+        last_name=argv[3],
+        email=argv[4],
+        password=argv[5]
     )
-    subject.save()
+    user.save()
